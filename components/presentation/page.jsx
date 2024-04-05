3f16af527c81c5b5941014076c8fcba842a10c06
zunniga <jmoficial2003@gmail.com>
2024-04-05 00:55:44 -0500

    change in the world.png

--- components/presentation/page.jsx
+++ components/presentation/page.jsx
@@ -1,9 +1,21 @@
 "use client"
 import Link from 'next/link';
 import Image from 'next/image'
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import Count from '@/components/common/count';
+import React, { useEffect, useRef, useState } from 'react';
+import service_data from '@/components/data/service-data';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import "./Presentation.css";
+// svg icon
+import AngleArrow from '@/svg/angle-arrow';
+import LineArrowTwo from '@/svg/line-arrow-2';
+import GreenRightArrow from '@/svg/green-right-arrow';
 
-
+// fun fact shape
+import fun_fact_shape_1 from "@/public/world/shape-1.png";
+import fun_fact_shape_2 from "@/public/world/shape-2.png";
+import fun_fact_shape_3 from "@/public/world/shape-3.png";
+import fun_fact_shape_4 from "@/public/world/shape-4.png";
 
 
 import reload_img from "@/public/img/banners/planeta.png";
@@ -74,74 +86,88 @@ const ServiceArea = () => {
       reloadClassName.style.transform = `rotate(${window.pageYOffset / 2}deg)`;
    };
 
-  
+   useEffect(() => {
+      if (reloadClassName !== null) {
+         window.addEventListener('scroll', scrollRotate);
+      }
+      return () => {
+         window.removeEventListener('scroll', scrollRotate);
+      };
+   }, );
 
-   
+
     return (   
         <>
-            <div className="tp-service-funfact-box "> 
+            <div className="tp-service-funfact-box flex flex-col md:flex-row justify-center items-center mb-20"> {/* Aplicamos flexbox para disposición horizontal y centrado vertical y horizontal */}
+   <section className="tp-fun-fact-area pt-60 pb-65 p-relative">
+      <div className="container container-1400">
 
-               <section className="tp-service-area pt-85 pb-50">
-                  <div className="container-fluid">
-                     
+         <div className="tp-fun-fact-shape">
+            <Image className="shape-1" src={fun_fact_shape_1} alt="theme-pure" />
+            <Image className="shape-2" src={fun_fact_shape_2} alt="theme-pure" />
+            <Image className="shape-3" src={fun_fact_shape_3} alt="theme-pure" />
+            <Image className="shape-4" src={fun_fact_shape_4} alt="theme-pure" />
+         </div>
 
-                     
-
-                  </div>
-               </section> 
-
-               <section className="tp-fun-fact-area pt-80 pb-65 p-relative">
-                  <div className="container container-1400">
-
-                     
-
-                     <div className="row">
-                        <div className="col-lg-2 col-md-4">
-                           
-                        </div>
-                        <div className="col-lg-4 col-md-8">
-                           <div className="tp-fun-fact-thumb p-relative">
-                              <Image id="reload" src={reload_img} alt="scroll" />
-                           </div>
-                        </div>
-
-                        <div className="col-lg-6 col-md-12">
-                           <div className="tp-fun-fact-content">
-                              <div className="tp-fun-fact-title-wrapper">
-                                 <span className="tp-section-title__pre">
-                                    service <span className="title-pre-color">IT Solutions</span> 
-                                  
-                                 </span>
-                                 <h3 className="tp-section-title">UN MUNDO COMPLICADO Y DESAFIANTE
-                                    <span className="title-left-shape"> 
-                                 
-                                    </span>
-                                 </h3>
-                                 <p>
-                                    Our company provides a full range of services for the construction of <br /> private houses and cottages since 19
-                                 </p>
-                                 <ul>
-                                    <li>
-                                     
-                                       series of manual and semi-manual activities.
-                                    </li>
-                                    <li>
-                                    
-                                       onstruction is different from other industries.
-                                    </li>
-                                 </ul>
-                                 <div className="tp-fun-fact-btn">
-                                    <Link className="tp-btn" href="/service-details">Tell us How Can We Help</Link>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-
-                  </div>
-               </section>  
-               
+         <div className="md:flex justify-center"> {/* Aplicamos flexbox y centrado horizontal para elementos internos */}
+            <div className="md:w-1/3">
+               <div className="tp-fun-fact-wrapper-box">
+                  {counter_content.map((item, i) => 
+                     <div key={i} className="tp-fun-fact-wrapper">
+                        <h3 className={`counter-title ${item.cls}`}> 
+                           <span data-purecounter-duration="4" className="purecounter">
+                              <Count number={item.count} text={item.icon} />
+                           </span>
+                        </h3>
+                        <p>{item.info}</p>
+                     </div>                              
+                  )} 
+               </div>
             </div>
+            <div className="md:w-1/3">
+               <div className="tp-fun-fact-thumb p-relative">
+                  <Image id="reload" src={reload_img} alt="scroll" />
+               </div>
+            </div>
+
+            <div className="md:w-1/3">
+               <div className="tp-fun-fact-content">
+                  <div className="tp-fun-fact-title-wrapper">
+                     <span className="tp-section-title__pre">
+                        service <span className="title-pre-color">IT Solutions</span> 
+                        <AngleArrow />
+                     </span>
+                     <h3 className="tp-section-title">UN MUNDO COMPLICADO Y DESAFIANTE
+                        <span className="title-left-shape"> 
+                     
+                        </span>
+                     </h3>
+                     <p>
+                        Our company provides a full range of services for the construction of <br /> private houses and cottages since 19
+                     </p>
+                     <ul>
+                        <li>
+                           <span> <GreenRightArrow /></span>
+                           series of manual and semi-manual activities.
+                        </li>
+                        <li>
+                           <span><GreenRightArrow /> </span>
+                           construction is different from other industries.
+                        </li>
+                     </ul>
+                     <div className="tp-fun-fact-btn">
+                        <Link className="tp-btn" href="/service-details">Tell us How Can We Help</Link>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+      </div>
+   </section>  
+</div>
+
+
         </>
     );
 };
---
+++ svg/line-arrow-2.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LineArrowTwo = () => {
+  return (
+    <>
+      <svg
+        width="194"
+        height="5"
+        viewBox="0 0 194 5"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M193.649 4.22307C129.517 2.55338 65.7543 2.288 1.66891 4.99709C-0.571625 5.09661 -0.540924 2.60867 1.66891 2.48703C65.34 -0.962917 130.116 -1.06243 193.649 3.70336C194.109 3.73653 194.125 4.23412 193.649 4.22307Z"
+          fill="#05DAC3"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default LineArrowTwo;
